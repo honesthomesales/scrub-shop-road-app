15fff9cd74579f0b15921bd47a72c56a3f7f79e9
honesthomesales <honesthomesales@users.noreply.github.com>
2025-12-25 03:04:45 +0000

    deploy: d77ef2bca7baa4b69c856f8d966afe5eba551115

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/scrub-shop-road-app/sw.js', { scope: '/scrub-shop-road-app/' })})}
